e38a76cff7d6404ca04b273df3647ad60585db68
Red Wil <redwil@gmail.com>
2025-12-11 15:33:37 -0500

    Force RPL-classic in border-router example

--- examples/rpl-border-router/project-conf.h
+++ examples/rpl-border-router/project-conf.h
@@ -53,6 +53,8 @@
 #ifndef CONTIKI_TARGET_NATIVE
 /* Ship logs over the border-router link (default SLIP) with "!L" prefix while keeping stdout. */
 #define BR_LOG_CONF_ENABLE 1
+/* Force RPL-classic so RPL hooks emit !R events. */
+#define BR_CONF_FORCE_RPL_CLASSIC 1
 /* Optional: select COBS framing instead of SLIP for all link traffic.
  * #define BR_CONF_LINK_FRAMING BR_LINK_FRAMING_COBS
  */
